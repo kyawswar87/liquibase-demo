9f7bc9768713b804b8c7d2042dfb06ccf27879e6
kyawswaraung <kyawswar.aung@cdsg.com.mm>
2023-06-19 16:41:46 +0630

    initial commit

---
+++ example-changelog.sql
@@ -0,0 +1,29 @@
+--liquibase formatted sql
+
+--changeset kyawswar:1 labels:example-label context:example-context
+--comment: example comment
+create table person (
+    id int primary key auto_increment not null,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
+--rollback DROP TABLE person;
+
+--changeset kyawswar:2 labels:example-label context:example-context
+--comment: example comment
+create table company (
+    id int primary key auto_increment not null,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
+--rollback DROP TABLE company;
+
+--changeset kyawswar:3 labels:example-label context:example-context
+--comment: example comment
+alter table person add column country varchar(2)
+--rollback ALTER TABLE person DROP COLUMN country;
+
